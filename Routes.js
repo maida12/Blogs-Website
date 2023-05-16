6027b1af7d6117c1388ee7ce4a9c8a5ac66ad572
Maida <maidaa.shahid@gmail.com>
2023-05-17 00:43:48 +0500

    Final Commit with comments added

--- Routes.js
+++ Routes.js
@@ -15,8 +15,9 @@ app.use(
 );
 
 
-const url =
-  "mongodb+srv://Samreen:4524.smr@cluster0.0vamaay.mongodb.net/Blog?retryWrites=true&w=majority";
+const url ="mongodb://l201377:smO5EOidmX5m72lv@ac-qiizi1h-shard-00-00.vdngowi.mongodb.net:27017,ac-qiizi1h-shard-00-01.vdngowi.mongodb.net:27017,ac-qiizi1h-shard-00-02.vdngowi.mongodb.net:27017/?ssl=true&replicaSet=atlas-stl8nf-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+  //"mongodb+srv://Samreen:4524.smr@cluster0.0vamaay.mongodb.net/Blog?retryWrites=true&w=majority";
 
 // Connect to MongoDB
 mongoose
@@ -223,3 +224,118 @@ app.put("/blog-posts/:id", async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 });
+
+// Delete a blog post
+app.delete("/blog-posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userId = req.session.userId;
+	
+	// Check if the user is authenticated
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    // Find the blog post by ID and author
+    const blogPost = await BlogPost.findOneAndDelete({
+      _id: id,
+      author: userId,
+    });
+
+    if (!blogPost) {
+      return res
+        .status(404)
+        .json({ message: "Blog post not found or unauthorized" });
+    }
+
+    return res.status(200).json({ message: "Blog post deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
+//===============================================================================================================
+
+// Comment Schema
+const comment = new mongoose.Schema({
+  content: { type: String, required: true },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  blogPost: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "BlogPost",
+    required: true,
+  },
+  date: { type: Date, default: Date.now },
+});
+
+const Comment = mongoose.model("Comment", comment);
+
+
+// Add comment
+app.post("/blog-posts/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { content } = req.body;
+    const author = req.session.userId;
+	
+	
+	// Check if the user is authenticated
+    if (!author) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Find the blog post by ID
+    const blogPost = await BlogPost.findById(id);
+
+    if (!blogPost) {
+      return res.status(404).json({ message: "Blog post not found" });
+    }
+
+    // Create a new comment
+    const newComment = new Comment({
+      content,
+      author,
+      blogPost: blogPost._id,
+    });
+    console.log(newComment);
+    await newComment.save();
+
+    // Add the comment to the blog post's comments array
+    blogPost.comments.push(newComment._id);
+    await blogPost.save();
+
+    return res.status(201).json({ message: "Comment added successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// read comment
+app.get("/blog-posts/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Find the blog post by ID and populate the comments
+    const blogPost = await BlogPost.findById(id).populate("comments");
+
+    if (!blogPost) {
+      return res.status(404).json({ message: "Blog post not found" });
+    }
+
+    return res.status(200).json(blogPost.comments);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+// Start the server
+app.listen(3000, () => {
+  console.log("Server is running on port 8080");
+});
+
